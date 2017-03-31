5229b5c6395cf6e75f142e371382f39dc1e548d4
Katulobotomia <joonas.natynki@gmail.com>
2017-03-31 15:00:42 +0300

    First Commit

---
+++ Sleepy/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Sleepy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.inputfield = new System.Windows.Forms.TextBox();
+            this.addfiveminutes = new System.Windows.Forms.Button();
+            this.timelefttext = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start countdown";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 90);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(398, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // inputfield
+            // 
+            this.inputfield.Location = new System.Drawing.Point(169, 14);
+            this.inputfield.Name = "inputfield";
+            this.inputfield.Size = new System.Drawing.Size(58, 20);
+            this.inputfield.TabIndex = 4;
+            this.inputfield.Text = "?";
+            this.inputfield.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.inputfield.TextChanged += new System.EventHandler(this.inputfield_TextChanged);
+            this.inputfield.Enter += new System.EventHandler(this.inputfield_Enter);
+            // 
+            // addfiveminutes
+            // 
+            this.addfiveminutes.Location = new System.Drawing.Point(297, 45);
+            this.addfiveminutes.Name = "addfiveminutes";
+            this.addfiveminutes.Size = new System.Drawing.Size(75, 23);
+            this.addfiveminutes.TabIndex = 5;
+            this.addfiveminutes.Text = "+ 5 min";
+            this.addfiveminutes.UseVisualStyleBackColor = true;
+            this.addfiveminutes.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timelefttext
+            // 
+            this.timelefttext.AutoSize = true;
+            this.timelefttext.Location = new System.Drawing.Point(309, 71);
+            this.timelefttext.Name = "timelefttext";
+            this.timelefttext.Size = new System.Drawing.Size(49, 13);
+            this.timelefttext.TabIndex = 6;
+            this.timelefttext.Text = "...timeleft";
+            this.timelefttext.Click += new System.EventHandler(this.timelefttext_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(233, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "minutes";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Shutdown";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 35);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(53, 17);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "Sleep";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "...in";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(423, 131);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.timelefttext);
+            this.Controls.Add(this.addfiveminutes);
+            this.Controls.Add(this.inputfield);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sleepy";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox inputfield;
+        private System.Windows.Forms.Button addfiveminutes;
+        private System.Windows.Forms.Label timelefttext;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Sleepy/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Sleepy
+{
+    public partial class Form1 : Form
+    {
+        int timeleft;
+        string time;
+
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar1.Maximum = 100;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timeleft = Int32.Parse(time) * 60;
+            progressBar1.Maximum = timeleft;   // Set progress bar maximum to inputted time
+            timer1.Start(); // Start the timer
+            timelefttext.Text = timeleft.ToString() + " seconds left!";   // Change the textbox text immediately
+            progressBar1.Value = 0; // Start from beginning
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timelefttext.Text = timeleft.ToString() + " seconds left!";    // Update time left
+            progressBar1.Increment(1);
+            timeleft--;
+
+            if(timeleft == 0)
+            {
+                //  SHUTDOWN!!
+                if(checkBox1.Checked)
+                {
+                    Process.Start("shutdown", "/s /t 0");   // Shuts down the computer
+                }
+                //  SLEEP!!!
+                if(checkBox2.Checked)
+                {
+                    Application.SetSuspendState(PowerState.Suspend, false, false);  // Puts the computer to sleep
+                }
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inputfield_TextChanged(object sender, EventArgs e)
+        {
+            time = inputfield.Text;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timeleft = timeleft + 300;
+            timelefttext.Text = timeleft.ToString() + " seconds left!";
+            progressBar1.Maximum = progressBar1.Maximum + 300;
+        }
+
+        private void inputfield_Enter(object sender, EventArgs e)
+        {
+            inputfield.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timelefttext_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //  If shutdown, no sleep
+            if(checkBox1.Checked)
+            {
+                checkBox2.Checked = false;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            //  If sleep, no shutdown
+            if(checkBox2.Checked)
+            {
+                checkBox1.Checked = false;
+            }
+        }
+    }
+}
